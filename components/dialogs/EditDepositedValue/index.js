9a4528e7ef155b0dcbdb9cd32aa639eeb1ab30de
Caio Graça Melo <caio.g.melo@hotmail.com>
2022-09-07 16:58:49 -0300

    Sending the deposit value as a float instead of a string

--- components/dialogs/EditDepositedValue/index.js
+++ components/dialogs/EditDepositedValue/index.js
@@ -1,18 +1,19 @@
 export default {
-  name: 'EditDepositedValueDialog',
-  props: {
-    depositedValueProp: Number
-  },
-  data: () => ({
-    depositedValue: 0,
-  }),
-  created() {
-    this.depositedValue = this.depositedValueProp
-  },
-  methods: {
-    async update_value() {
-      await this.$axios.put(`config/update-deposited-value`, { deposited: this.depositedValue })
-      return this.$emit('update', parseFloat(this.depositedValue));
+    name: 'EditDepositedValueDialog',
+    props: {
+        depositedValueProp: Number
+    },
+    data: () => ({
+        depositedValue: 0,
+    }),
+    created() {
+        this.depositedValue = this.depositedValueProp
+    },
+    methods: {
+        async update_value() {
+            const deposited = parseFloat(this.depositedValue)
+            await this.$axios.put(`config/update-deposited-value`, { deposited })
+            return this.$emit('update', deposited);
+        }
     }
-  }
 }
