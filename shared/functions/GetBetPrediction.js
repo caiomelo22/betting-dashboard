ec0fd092d2f75287633cf45c1d2a10c9266a5a5c
Caio Graça Melo <caio.g.melo@hotmail.com>
2022-10-10 22:14:42 -0300

    Display moneyline bets that include a draw possibility

--- shared/functions/GetBetPrediction.js
+++ shared/functions/GetBetPrediction.js
@@ -1,7 +1,7 @@
 export const get_bet_prediction = (bet) => {
     switch (bet.type) {
         case 'Moneyline':
-            return bet.moneyline.prediction;
+            return `${bet.moneyline.prediction}${bet.moneyline.includeDraw ? '/Draw' : ''}`;
         case 'Total':
             return `${bet.total.prediction} ${bet.total.line}`;
         case 'BothScore':
