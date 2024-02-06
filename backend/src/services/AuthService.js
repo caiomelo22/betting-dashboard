2df6d66b833f9d940ea192ddcb987c5c9c0f81a7
Caio Graça Melo <caio.g.melo@hotmail.com>
2024-02-06 17:43:50 -0300

    Changing the lifespan of the access and refresh tokens

--- backend/src/services/AuthService.js
+++ backend/src/services/AuthService.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (user) => {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1w' })
 }
 
 const generateLoginTokens = (user) => {
     const accessToken = generateAccessToken({email: user.email})
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1w' })
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' })
 
     return {accessToken, refreshToken}
 }
