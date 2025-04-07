e77db777cf1ecf7c870ce48bff9eaf1a86b9ca46
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-04-06 23:49:32 -0300

    Improvements to the auth pages

--- frontend/pages/Auth/Login/index.vue
+++ frontend/pages/Auth/Login/index.vue
@@ -1,18 +1,23 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <form @submit.prevent="registerUser">
-            <!-- Add your registration form fields here -->
-            <div>
-                <label for="username">Username:</label>
-                <input v-model="username" type="text" required />
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input v-model="password" type="password" required />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+    <div style="display: flex; justify-content: center;">
+        <div style="width: fit-content;">
+            <form @submit.prevent="registerUser">
+                <!-- Add your registration form fields here -->
+                <div>
+                    <label for="username">Username:</label>
+                    <input v-model="username" class="auth-field" type="text" required />
+                </div>
+                <div>
+                    <label for="password">Password:</label>
+                    <input v-model="password" class="auth-field" type="password" required />
+                </div>
+                <div style="display: flex; justify-content: center; margin: 12px 0px">
+                    <v-btn color="green" class="dialog-btn" type="submit" :loading="loading" dark>
+                        Login
+                    </v-btn>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 <script src="./index"></script>
